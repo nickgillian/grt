740d5f72a89a729fa59981115d8c01dab858b162
Nick <nick@nickgillian.com>
2015-04-01 20:47:14 +0100

    added validate option to CommandLineParser

--- GRT/Util/CommandLineParser.h
+++ GRT/Util/CommandLineParser.h
@@ -33,12 +33,12 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <fstream>
 #include <typeinfo>
 #include "InfoLog.h"
+#include "WarningLog.h"
 #include "ErrorLog.h"
-#include "Util.h"
 
 namespace GRT{
 
-class CommandLineParser{
+class CommandLineParser {
 public:
     /**
      Default constructor.
@@ -48,13 +48,35 @@ public:
     /**
      Default destructor.
     */
-    ~CommandLineParser(){}
+    virtual ~CommandLineParser(){}
     
-    bool addOption(const std::string option,const std::string destination){
-        options[option] = destination;
+    /**
+     Adds an option with a specific retrival id. For example, addOption("-f","filename") would add the option "-f" which can be retrived using the "filename" id.
+
+     @param const std::string &option: the command line option you want to store when the command line is parsed
+     @param const std::string &id: the id that will be used to retrieve the data associated with the option
+     @return returns true if the option was added succesfully, false otherwise
+    */
+    bool addOption(const std::string &option,const std::string &id){
+
+        //Make sure the option has not already been added
+        std::map<std::string,std::string>::iterator it = options.find( option );
+        if( it != options.end() ){
+            return false;
+        }
+
+        //Add the option and id
+        options[option] = id;
         return true;
     }
     
+    /**
+     Parses the command line arguments, storing any relevant results in the results buffer. The can then be accessed using the get(...) function.
+
+     @param const int argc: the number of command line arguments
+     @param const char * argv[]: an array of arguments
+     @return returns true if the command line was parsed succesfully, false otherwise
+    */
     bool parse(const int argc,const char * argv[] ){
         
         results.clear();
@@ -72,30 +94,96 @@ public:
         return true;
     }
     
-    template<class T> bool get(const std::string destination,T &option,T defaultValue = T()){
+    /**
+     Retrives the value associated with the relevant id and stores it in value.
+     If the id is invalid or the type can not be parsed, then the function will return false.
+
+     @param const std::string &id: the search id
+     @param T &value: stores the return value
+     @param T defaultValue = T(): sets the default return value if the id is not found
+     @return returns true if the value associated with the id was found succesfully, false otherwise
+    */
+    template<class T> bool get(const std::string &id,T &value,T defaultValue = T()){
         
         //Find the option in the results buffer
         std::map<std::string,std::string>::iterator it;
-        it = results.find( destination );
+        it = results.find( id );
         
         if( it == results.end() ){
-		option = defaultValue;
-            	warningLog << "get(const std::string destination,T &option) - Failed to find destination: " << destination << endl;
-            	return false;
+            value = defaultValue;
+            warningLog << "get(const std::string &id,T &value) - Failed to find id: " << id << endl;
+            return false;
         }
         
+        //Convert the data associated with the id to the relevant type
         try{
-            	std::stringstream s( it->second );
-           	 s >> option;
-		return true;
+            std::stringstream s( it->second );
+            s >> value;
+            return true;
         }catch( ... ){
-		warningLog << "get(const std::string destination,T &option) - Can not parse type: " << typeid( option ).name() << endl;
+            value = defaultValue;
+            warningLog << "get(const std::string &id,T &value) - Can not parse type: " << typeid( value ).name() << endl;
         }
 
         return false;
     }
     
-    
+    /**
+     Validates if id was succesfully parsed from the command line.
+     If the id is invalid or was not parsed, then the function will return false.
+
+     @param const std::string &id: the search id
+     @return returns true if the value associated with the id was parsed succesfully, false otherwise
+    */
+    bool validate(const std::string &id){
+        
+        //Find the option in the results buffer
+        std::map<std::string,std::string>::iterator it;
+        it = results.find( id );
+        
+        if( it == results.end() ){
+            warningLog << "validate(const std::string ) - Failed to find id: " << id << endl;
+            return false;
+        }
+        
+        //If we get this far then we've found a match
+        return true;
+    }
+
+    /**
+     Sets if info logging is enabled/disabled for this specific instance.
+     If you want to enable/disable info logging globally, then you should use the InfoLog::enableLogging( bool ) function.
+
+     @return returns true if the parameter was updated, false otherwise
+    */
+    bool setInfoLoggingEnabled(const bool loggingEnabled){
+        infoLog.setEnableInstanceLogging( loggingEnabled );
+        return true;
+    }
+
+    /**
+     Sets if warning logging is enabled/disabled for this specific instance.
+     If you want to enable/disable warning logging globally, then you should use the WarningLog::enableLogging( bool ) function.
+
+     @return returns true if the parameter was updated, false otherwise
+     */
+    bool setWarningLoggingEnabled(const bool loggingEnabled){
+        warningLog.setEnableInstanceLogging( loggingEnabled );
+        return true;
+    }
+
+    /**
+     Sets if error logging is enabled/disabled for this specific instance.
+     If you want to enable/disable error logging globally, then you should use the ErrorLog::enableLogging( bool ) function.
+
+     @return returns true if the parameter was updated, false otherwise
+    */
+    bool setErrorLoggingEnabled(const bool loggingEnabled){
+        errorLog.setEnableInstanceLogging( loggingEnabled );
+        return true;
+    }
+
+protected:
     InfoLog infoLog;
     WarningLog warningLog;
     ErrorLog errorLog;
