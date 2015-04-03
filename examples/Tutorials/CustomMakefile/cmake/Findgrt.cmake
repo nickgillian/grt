6f37942f37461f73f1381e6d08f9a8a6adf5a8d6
Nick <nick@nickgillian.com>
2015-04-03 11:43:39 +0100

    updated custom makefile to support C++11

--- examples/Tutorials/CustomMakefile/cmake/Findgrt.cmake
+++ examples/Tutorials/CustomMakefile/cmake/Findgrt.cmake
@@ -25,5 +25,18 @@ MARK_AS_ADVANCED(
 SET(GRT_INCLUDE_DIRS "${GRT_INCLUDE_DIR}")
 SET(GRT_LIBRARIES "${GRT_LIBRARY}")
 
+#Add any custom OSX definitions
+if(APPLE)
+    #Add C++11 and multithreading support
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -stdlib=libc++")
+else()
+	
+#Add any custom Linux definitions
+if(UNIX) 
+    #Add C++11 and multithreading support
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread -std=c++11")
+endif()
+endif() #end of else(APPLE)
+
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(GRT DEFAULT_MSG GRT_LIBRARIES GRT_INCLUDE_DIRS)
