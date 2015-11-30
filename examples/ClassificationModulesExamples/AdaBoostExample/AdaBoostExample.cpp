aa0df9334fc7e05f3584e60f89a8023b8da40d75
Nick Gillian <ngillian@google.com>
2015-11-29 21:25:21 -0500

    updated adaboost example, removed training data

--- examples/ClassificationModulesExamples/AdaBoostExample/AdaBoostExample.cpp
+++ examples/ClassificationModulesExamples/AdaBoostExample/AdaBoostExample.cpp
@@ -42,10 +42,12 @@
  This example shows you how to:
  - Create an initialize the AdaBoost algorithm
  - Set a DecisionStump as the WeakClassifer
- - Load some LabelledClassificationData from a file and partition the training data into a training dataset and a test dataset
+ - Load some ClassificationData from a file and partition the data into a training dataset and a test dataset
  - Train the AdaBoost algorithm using the training dataset
  - Test the AdaBoost algorithm using the test dataset
  - Manually compute the accuracy of the classifier
+
+ You should run this example with one argument pointing to the data you want to load. A good dataset to run this example is acc-orientation.grt, which can be found in the GRT data folder.
 */
 
 //You might need to set the specific path of the GRT header relative to your project
@@ -54,6 +56,13 @@ using namespace GRT;
 
 int main (int argc, const char * argv[])
 {
+    //Parse the data filename from the argument list
+    if( argc != 2 ){
+        cout << "Error: failed to parse data filename from command line. You should run this example with one argument pointing to the data filename!\n";
+        return EXIT_FAILURE;
+    }
+    const string filename = argv[1];
+
     //Create a new AdaBoost instance
     AdaBoost adaBoost;
 
@@ -63,8 +72,8 @@ int main (int argc, const char * argv[])
     //Load some training data to train the classifier
     ClassificationData trainingData;
 
-    if( !trainingData.load("AdaBoostTrainingData.grt") ){
-        cout << "Failed to load training data!\n";
+    if( !trainingData.load( filename ) ){
+        cout << "Failed to load training data: " << filename << endl;
         return EXIT_FAILURE;
     }
 
