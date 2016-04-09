6167b3e5ee0d1a7e3e39337f83c535d021ebec0e
Nick Gillian <nick@nickgillian.com>
2016-04-08 22:35:27 -0700

    added envelope extractor feature extraction

---
+++ GRT/FeatureExtractionModules/EnvelopeExtractor/EnvelopeExtractor.h
@@ -0,0 +1,145 @@
+/**
+ @file
+ @author  Nicholas Gillian <ngillian@media.mit.edu>
+ @version 1.0
+ 
+ @brief
+ */
+
+/**
+ GRT MIT License
+ Copyright (c) <2012> <Nicholas Gillian, Media Lab, MIT>
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial
+ portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef GRT_ENVELOPE_EXTRACTOR_HEADER
+#define GRT_ENVELOPE_EXTRACTOR_HEADER
+
+//Include the main GRT header to get access to the FeatureExtraction base class
+#include "../../CoreModules/FeatureExtraction.h"
+#include "../../DataStructures/TimeSeriesClassificationData.h"
+#include "../../DataStructures/ClassificationDataStream.h"
+#include "../../DataStructures/UnlabelledData.h"
+
+GRT_BEGIN_NAMESPACE
+    
+class EnvelopeExtractor : public FeatureExtraction{
+public:
+    /**
+     Default constructor. Initalizes the EnvelopeExtractor, setting the number of input dimensions and the number of clusters to use in the quantization model.
+	
+	@param numDimensions: the number of dimensions in the input data
+    @param numClusters: the number of quantization clusters
+     */
+    EnvelopeExtractor(const UINT bufferSize = 100,const UINT numDimensions = 1);
+	
+    /**
+     Copy constructor, copies the KMeansQuantizer from the rhs instance to this instance.
+     
+     @param rhs: another instance of this class from which the data will be copied to this instance
+     */
+    EnvelopeExtractor(const EnvelopeExtractor &rhs);
+    
+    /**
+     Default Destructor
+     */
+    virtual ~EnvelopeExtractor();
+    
+    /**
+     Sets the equals operator, copies the data from the rhs instance to this instance.
+     
+     @param rhs: another instance of this class from which the data will be copied to this instance
+     @return a reference to this instance
+     */
+    EnvelopeExtractor& operator=(const EnvelopeExtractor &rhs);
+
+    /**
+     Sets the FeatureExtraction deepCopyFrom function, overwriting the base FeatureExtraction function.
+     This function is used to deep copy the values from the input pointer to this instance of the FeatureExtraction module.
+     This function is called by the GestureRecognitionPipeline when the user adds a new FeatureExtraction module to the pipeleine.
+     
+     @param featureExtraction: a pointer to another instance of this class, the values of that instance will be cloned to this instance
+     @return returns true if the deep copy was successful, false otherwise
+     */
+    virtual bool deepCopyFrom(const FeatureExtraction *featureExtraction);
+    
+    /**
+     Sets the FeatureExtraction computeFeatures function, overwriting the base FeatureExtraction function.
+     This function is called by the GestureRecognitionPipeline when any new input data needs to be processed (during the prediction phase for example).
+     This is where you should add your main feature extraction code.
+     
+     @param inputVector: the inputVector that should be processed.  Must have the same dimensionality as the FeatureExtraction module
+     @return returns true if the data was processed, false otherwise
+     */
+    virtual bool computeFeatures(const VectorFloat &inputVector);
+    
+    /**
+     Sets the FeatureExtraction reset function, overwriting the base FeatureExtraction function.
+     This function is called by the GestureRecognitionPipeline when the pipelines main reset() function is called.
+     You should add any custom reset code to this function to define how your feature extraction module should be reset.
+     
+     @return true if the instance was reset, false otherwise
+     */
+    virtual bool reset();
+    
+    /**
+     This saves the feature extraction settings to a file.
+     
+     @param filename: the filename to save the settings to
+     @return returns true if the settings were saved successfully, false otherwise
+     */
+    virtual bool saveModelToFile( std::string filename ) const;
+    
+    /**
+     This saves the feature extraction settings to a file.
+     
+     @param file: a reference to the file to save the settings to
+     @return returns true if the settings were saved successfully, false otherwise
+     */
+    virtual bool loadModelFromFile( std::string filename );
+    
+    /**
+     This saves the feature extraction settings to a file.
+     This overrides the saveSettingsToFile function in the FeatureExtraction base class.
+     You should add your own custom code to this function to define how your feature extraction module is saved to a file.
+     
+     @param file: a reference to the file to save the settings to
+     @return returns true if the settings were saved successfully, false otherwise
+     */
+    virtual bool saveModelToFile( std::fstream &file ) const;
+    
+    /**
+     This loads the feature extraction settings from a file.
+     This overrides the loadSettingsFromFile function in the FeatureExtraction base class.
+     
+     @param file: a reference to the file to load the settings from
+     @return returns true if the settings were loaded successfully, false otherwise
+     */
+    virtual bool loadModelFromFile( std::fstream &file );
+    
+    bool init( const UINT bufferSize = 100,const UINT numDimensions = 1 );
+    
+protected:
+    UINT bufferSize;
+    CircularBuffer< VectorFloat > buffer;
+    
+    static RegisterFeatureExtractionModule< EnvelopeExtractor > registerModule;
+};
+
+GRT_END_NAMESPACE
+
+#endif //GRT_KMEANS_FEATURES_HEADER
