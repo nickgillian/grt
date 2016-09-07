05947b7d87ac9f199ff7cd94f8779eb58c4d976a
Nick Gillian <ngillian@google.com>
2016-09-06 22:24:10 -0700

    updated logistic regression tool

--- tools/grt-log-reg-tool.cpp
+++ tools/grt-log-reg-tool.cpp
@@ -16,18 +16,32 @@
 using namespace GRT;
 using namespace std;
 
+const char *help = "This tool can be used for training a Softmax classification model.\n"
+                   "The dataset used to train the model can be in two format, (1) a GRT ClassificationData formatted file or (2) a CSV formatted file.\n"
+                   "If the data is formatted as a CSV file, then it should be formatted as follows:\n"
+                     "\t- each row should contain a sample\n"
+                     "\t- the first column should contain the class label\n"
+                     "\t- the remaining N columns should contain the input attributes (a.k.a. features)\n"
+                     "\t- columns should be seperated by a comma delimiter \',\'\n"
+                     "\t- rows should be terminated by a new line operator \'\\n\'";
+                                          
 InfoLog infoLog("[grt-log-reg-tool]");
 WarningLog warningLog("[WARNING grt-log-reg-tool]");
 ErrorLog errorLog("[ERROR grt-log-reg-tool]");
 
-bool printUsage(){
-    infoLog << "grt-log-reg-tool [options]\n";
-    infoLog << "\t-f: sets the filename the training data will be loaded from. The training data can either be a GRT RegressionData file or a CSV file. \n";
-    infoLog << "\t-n: sets the number of input dimensions in the dataset, only required if the input data format is a CSV file. \n";
-    infoLog << "\t-t: sets the number of target dimensions in the dataset, only required if the input data format is a CSV file. \n";
-    infoLog << "\t--model: sets the filename the regression model will be saved to\n";
-    infoLog << endl;
-    return true;
+bool printHelp(){
+  infoLog << "\nusage: grt-log-reg-tool [options]\n";
+  infoLog << "\t--help: prints this help message\n";
+  infoLog << "\t-f: sets the filename the training data will be loaded from. The training data can either be a GRT RegressionData file or a CSV file (see help below). \n";
+  infoLog << "\t--model: sets the filename the model will be saved to\n";
+  infoLog << "\t--batch-size: sets the number of training samples used in each batch update of the learning algorithm (default: 50)\n";
+  infoLog << "\t--learning-rate: sets the rate at which the learning algorithm will update the model weights at each batch update (default: 0.01)\n";
+  infoLog << "\t--min-change: sets the minimum change needed to signal convergence of the learning algorithm (default: 0.001)\n";
+  infoLog << "\t--max-epoch: sets the maximum number of epochs that the learning algorithm can run for, an epoch is one iteration over the entire training dataset (default: 500)\n";
+  infoLog << "\t--log-file: \n";
+  infoLog << "\nhelp:\n" << help;
+  infoLog << endl;
+  return true;
 }
 
 bool train( CommandLineParser &parser );
@@ -37,7 +51,7 @@ int main(int argc, char * argv[])
 
     if( argc < 2 ){
         errorLog << "Not enough input arguments!" << endl;
-        printUsage();
+        printHelp();
         return EXIT_FAILURE;
     }
 
@@ -49,9 +63,14 @@ int main(int argc, char * argv[])
 
     //Add some options and identifiers that can be used to get the results
     parser.addOption( "-f", "filename" );
-    parser.addOption( "-n", "num-input-dimensions" );
-    parser.addOption( "-t", "num-target-dimensions" );
-    parser.addOption( "--model", "model-filename" );
+    parser.addOption( "--model", "model-filename", "log-regression-model.grt" ); //Set the default filename to log-regression-model.grt
+    parser.addOption( "--num-inputs", "num-inputs" );
+    parser.addOption( "--num-targets", "num-targets" );
+    parser.addOption( "--batch-size", "batch-size", 50 ); //Set the default batch size to 50
+    parser.addOption( "--learning-rate", "learning-rate", 0.01 ); //Set the default learning rate to 0.01
+    parser.addOption( "--min-change", "min-change", 0.001 ); //Set the default min change to 0.001
+    parser.addOption( "--max-epoch", "max-epoch", 500 ); //Set the default max number of epochs to 500
+    parser.addOption( "--log", "log-filename" );
 
     //Parse the command line
     parser.parse( argc, argv );
@@ -63,7 +82,7 @@ int main(int argc, char * argv[])
     }
 
     errorLog << "Failed to train model!" << endl;
-    printUsage();
+    printHelp();
 
     return EXIT_FAILURE;
 }
@@ -74,17 +93,26 @@ bool train( CommandLineParser &parser ){
 
     string trainDatasetFilename = "";
     string modelFilename = "";
-    string defaultFilename = "linear-regression-model.grt";
+    float learningRate = 0;
+    float minChange = 0;
+    unsigned int maxEpoch = 0;
+    unsigned int batchSize = 0;
 
     //Get the filename
     if( !parser.get("filename",trainDatasetFilename) ){
         errorLog << "Failed to parse filename from command line! You can set the filename using the -f." << endl;
-        printUsage();
+        printHelp();
         return false;
     }
+    
+    //Get the parameters from the parser
+    parser.get("model-filename",modelFilename);
+    parser.get( "learning-rate", learningRate );
+    parser.get( "min-change", minChange );
+    parser.get( "max-epoch", maxEpoch );
+    parser.get( "batch-size", batchSize );
 
-    //Get the model filename
-    parser.get("model-filename",modelFilename,defaultFilename);
+    infoLog << "settings: learning-rate: " << learningRate << " min-change: " << minChange << " max-epoch: " << maxEpoch << " batch-size: " << batchSize << endl;
 
     //Load the training data to train the model
     RegressionData trainingData;
@@ -92,9 +120,15 @@ bool train( CommandLineParser &parser ){
     //Try and parse the input and target dimensions
     unsigned int numInputDimensions = 0;
     unsigned int numTargetDimensions = 0;
-    if( parser.get("num-input-dimensions",numInputDimensions) && parser.get("num-target-dimensions",numTargetDimensions) ){
+    if( parser.get("num-inputs",numInputDimensions) && parser.get("num-targets",numTargetDimensions) ){
       infoLog << "num input dimensions: " << numInputDimensions << " num target dimensions: " << numTargetDimensions << endl;
       trainingData.setInputAndTargetDimensions( numInputDimensions, numTargetDimensions );
+    }
+
+    if( (numInputDimensions == 0 || numTargetDimensions == 0) && Util::stringEndsWith( trainDatasetFilename, ".csv" ) ){
+      errorLog << "Failed to parse num input dimensions and num target dimensions from input arguments. You must supply the input and target dimensions if the data format is CSV!" << endl;
+      printHelp();
+      return false; 
     }
 
     infoLog << "- Loading Training Data..." << endl;
@@ -112,8 +146,8 @@ bool train( CommandLineParser &parser ){
     //Create a new regression instance
     LogisticRegression regression;
 
-    regression.setMaxNumEpochs( 500 );
-    regression.setMinChange( 1.0e-5 );
+    regression.setMaxNumEpochs( maxEpoch );
+    regression.setMinChange( minChange );
     regression.setUseValidationSet( true );
     regression.setValidationSetSize( 20 );
     regression.setRandomiseTrainingOrder( true );
