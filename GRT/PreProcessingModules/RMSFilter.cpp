5339d8c9eab9d0f18d500e779ac295ebcfe431ef
Nick <ngillian@google.com>
2016-09-28 21:30:42 -0700

    added RMS Filter

---
+++ GRT/PreProcessingModules/RMSFilter.cpp
@@ -0,0 +1,244 @@
+/*
+GRT MIT License
+Copyright (c) <2012> <Nicholas Gillian, Media Lab, MIT>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#define GRT_DLL_EXPORTS
+#include "RMSFilter.h"
+
+GRT_BEGIN_NAMESPACE
+
+//Register the RMS module with the PreProcessing base class
+RegisterPreProcessingModule< RMS > RMS::registerModule("RMSFilter");
+
+RMS::RMS(UINT filterSize,UINT numDimensions) : PreProcessing( "RMSFilter" )
+{
+    init(filterSize,numDimensions);
+}
+
+RMS::RMS(const RMS &rhs) : PreProcessing( "RMSFilter" )
+{ 
+    //Zero this instance
+    this->filterSize = 0;
+    this->inputSampleCounter = 0;
+    
+    //Copy the settings from the rhs instance
+    *this = rhs;
+}
+
+RMS::~RMS(){
+    
+}
+
+RMS& RMS::operator=(const RMS &rhs){
+    if(this!=&rhs){
+        //Clear this instance
+        this->filterSize = 0;
+        this->inputSampleCounter = 0;
+        this->dataBuffer.clear();
+        
+        //Copy from the rhs instance
+        if( rhs.initialized ){
+            this->init( rhs.filterSize, rhs.numInputDimensions );
+            this->dataBuffer = rhs.dataBuffer;
+        }
+        
+        //Copy the preprocessing base variables
+        copyBaseVariables( (PreProcessing*)&rhs );
+    }
+    return *this;
+}
+
+bool RMS::deepCopyFrom(const PreProcessing *preProcessing){
+    
+    if( preProcessing == NULL ) return false;
+    
+    if( this->getPreProcessingType() == preProcessing->getPreProcessingType() ){
+        
+        //Call the equals operator
+        *this = *dynamic_cast<RMS*>(preProcessing);
+        
+        return true;
+    }
+    
+    errorLog << "deepCopyFrom(const PreProcessing *preProcessing) -  PreProcessing Types Do Not Match!" << std::endl;
+    
+    return false;
+}
+
+
+bool RMS::process(const VectorFloat &inputVector){
+    
+    if( !initialized ){
+        errorLog << "process(const VectorFloat &inputVector) - The filter has not been initialized!" << std::endl;
+        return false;
+    }
+    
+    if( inputVector.getSize() != numInputDimensions ){
+        errorLog << "process(const VectorFloat &inputVector) - The size of the inputVector (" << inputVector.getSize() << ") does not match that of the filter (" << numInputDimensions << ")!" << std::endl;
+        return false;
+    }
+    
+    filter( inputVector );
+    
+    if( processedData.getSize() == numOutputDimensions ) return true;
+    
+    return false;
+}
+
+bool RMS::reset(){
+    if( initialized ) return init(filterSize,numInputDimensions);
+    return false;
+}
+
+bool RMS::save(std::fstream &file) const{
+    
+    if( !file.is_open() ){
+        errorLog << "save(fstream &file) - The file is not open!" << std::endl;
+        return false;
+    }
+    
+    file << "GRT_MOVING_AVERAGE_FILTER_FILE_V1.0" << std::endl;
+    
+    file << "NumInputDimensions: " << numInputDimensions << std::endl;
+    file << "NumOutputDimensions: " << numOutputDimensions << std::endl;
+    file << "FilterSize: " << filterSize << std::endl;
+    
+    return true;
+}
+
+bool RMS::load(std::fstream &file){
+    
+    if( !file.is_open() ){
+        errorLog << "load(fstream &file) - The file is not open!" << std::endl;
+        return false;
+    }
+    
+    std::string word;
+    
+    //Load the header
+    file >> word;
+    
+    if( word != "GRT_MOVING_AVERAGE_FILTER_FILE_V1.0" ){
+        errorLog << "load(fstream &file) - Invalid file format!" << std::endl;
+        return false;
+    }
+    
+    //Load the number of input dimensions
+    file >> word;
+    if( word != "NumInputDimensions:" ){
+        errorLog << "load(fstream &file) - Failed to read NumInputDimensions header!" << std::endl;
+        return false;
+    }
+    file >> numInputDimensions;
+    
+    //Load the number of output dimensions
+    file >> word;
+    if( word != "NumOutputDimensions:" ){
+        errorLog << "load(fstream &file) - Failed to read NumOutputDimensions header!" << std::endl;
+        return false;
+    }
+    file >> numOutputDimensions;
+    
+    //Load the filter factor
+    file >> word;
+    if( word != "FilterSize:" ){
+        errorLog << "load(fstream &file) - Failed to read FilterSize header!" << std::endl;
+        return false;
+    }
+    file >> filterSize;
+    
+    //Init the filter module to ensure everything is initialized correctly
+    return init(filterSize,numInputDimensions);
+}
+
+bool RMS::init(UINT filterSize,UINT numDimensions){
+    
+    //Cleanup the old memory
+    initialized = false;
+    inputSampleCounter = 0;
+    
+    if( filterSize == 0 ){
+        errorLog << "init(UINT filterSize,UINT numDimensions) - Filter size can not be zero!" << std::endl;
+        return false;
+    }
+    
+    if( numDimensions == 0 ){
+        errorLog << "init(UINT filterSize,UINT numDimensions) - The number of dimensions must be greater than zero!" << std::endl;
+        return false;
+    }
+    
+    //Resize the filter
+    this->filterSize = filterSize;
+    this->numInputDimensions = numDimensions;
+    this->numOutputDimensions = numDimensions;
+    processedData.clear();
+    processedData.resize(numDimensions,0);
+    initialized = dataBuffer.resize( filterSize, VectorFloat(numInputDimensions,0) );
+    
+    if( !initialized ){
+        errorLog << "init(UINT filterSize,UINT numDimensions) - Failed to resize dataBuffer!" << std::endl;
+    }
+    
+    return initialized;
+}
+
+Float RMS::filter(const Float x){
+    
+    //If the filter has not been initialised then return 0, otherwise filter x and return y
+    if( !initialized ){
+        errorLog << "filter(const Float x) - The filter has not been initialized!" << std::endl;
+        return 0;
+    }
+    
+    VectorFloat y = filter(VectorFloat(1,x));
+    
+    if( y.size() == 0 ) return 0;
+    return y[0];
+}
+
+VectorFloat RMS::filter(const VectorFloat &x){
+    
+    //If the filter has not been initialised then return 0, otherwise filter x and return y
+    if( !initialized ){
+        errorLog << "filter(const VectorFloat &x) - The filter has not been initialized!" << std::endl;
+        return VectorFloat();
+    }
+    
+    if( x.size() != numInputDimensions ){
+        errorLog << "filter(const VectorFloat &x) - The size of the input vector (" << x.getSize() << ") does not match that of the number of dimensions of the filter (" << numInputDimensions << ")!" << std::endl;
+        return VectorFloat();
+    }
+    
+    if( ++inputSampleCounter > filterSize ) inputSampleCounter = filterSize;
+    
+    //Add the new value to the buffer
+    dataBuffer.push_back( x );
+    
+    for(unsigned int j=0; j<numInputDimensions; j++){
+        processedData[j] = 0;
+        for(unsigned int i=0; i<inputSampleCounter; i++) {
+            processedData[j] += dataBuffer[i][j] * dataBuffer[i][j];
+        }
+        processedData[j] = sqrt( processedData[j] / Float(inputSampleCounter) );
+    }
+    
+    return processedData;
+}
+
+GRT_END_NAMESPACE
---
+++ GRT/PreProcessingModules/RMSFilter.h
@@ -0,0 +1,164 @@
+/**
+@file
+@author  Nicholas Gillian <ngillian@media.mit.edu>
+@version 1.0
+
+@brief The RMSFilter implements a root mean squared (RMS) filter.
+*/
+
+/**
+GRT MIT License
+Copyright (c) <2012> <Nicholas Gillian, Media Lab, MIT>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef GRT_RMS_FILTER_HEADER
+#define GRT_RMS_FILTER_HEADER
+
+#include "../CoreModules/PreProcessing.h"
+
+GRT_BEGIN_NAMESPACE
+
+class RMSFilter : public PreProcessing {
+public:
+    /**
+    Constructor, sets the size of the filter (the size of the buffer used to store and filter data) and the dimensionality of the data it will filter.
+    
+    @param filterSize: the size of the RMS filter buffer, should be a value greater than zero
+    @param numDimensions: the dimensionality of the data to filter
+    */
+    RMSFilter(UINT filterSize = 5,UINT numDimensions = 1);
+    
+    /**
+    Copy Constructor, copies the RMSFilter from the rhs instance to this instance
+    
+    @param rhs: another instance of the RMSFilter class from which the data will be copied to this instance
+    */
+    RMSFilter(const RMSFilter &rhs);
+    
+    /**
+    Default Destructor
+    */
+    virtual ~RMSFilter();
+    
+    /**
+    Sets the equals operator, copies the data from the rhs instance to this instance
+    
+    @param rhs: another instance of the RMSFilter class from which the data will be copied to this instance
+    @return a reference to this instance of RMSFilter
+    */
+    RMSFilter& operator=(const RMSFilter &rhs);
+    
+    /**
+    Sets the PreProcessing deepCopyFrom function, overwriting the base PreProcessing function.
+    This function is used to deep copy the values from the input pointer to this instance of the PreProcessing module.
+    This function is called by the GestureRecognitionPipeline when the user adds a new PreProcessing module to the pipeline.
+    
+    @param preProcessing: a pointer to another instance of a RMSFilter, the values of that instance will be cloned to this instance
+    @return true if the deep copy was successful, false otherwise
+    */
+    virtual bool deepCopyFrom(const PreProcessing *preProcessing);
+    
+    /**
+    Sets the PreProcessing process function, overwriting the base PreProcessing function.
+    This function is called by the GestureRecognitionPipeline when any new input data needs to be processed (during the prediction phase for example).
+    This function calls the RMSFilter's filter function.
+    
+    @param inputVector: the inputVector that should be processed.  Must have the same dimensionality as the PreProcessing module
+    @return true if the data was processed, false otherwise
+    */
+    virtual bool process(const VectorFloat &inputVector);
+    
+    /**
+    Sets the PreProcessing reset function, overwriting the base PreProcessing function.
+    This function is called by the GestureRecognitionPipeline when the pipelines main reset() function is called.
+    This function resets the filter values by re-initiliazing the filter.
+    
+    @return true if the filter was reset, false otherwise
+    */
+    virtual bool reset();
+    
+    /**
+    This saves the current settings of the RMSFilter to a file.
+    This overrides the save function in the PreProcessing base class.
+    
+    @param file: a reference to the file the settings will be saved to
+    @return returns true if the settings were saved successfully, false otherwise
+    */
+    virtual bool save(std::fstream &file) const;
+    
+    /**
+    This loads the RMSFilter settings from a file.
+    This overrides the load function in the PreProcessing base class.
+    
+    @param file: a reference to the file to load the settings from
+    @return returns true if the model was loaded successfully, false otherwise
+    */
+    virtual bool load(std::fstream &file);
+    
+    /**
+    Initializes the filter, setting the filter size and dimensionality of the data it will filter.
+    Sets all the filter values to zero.
+    
+    @param filterSize: the size of the moving average filter, should be a value greater than zero
+    @return true if the filter was initiliazed, false otherwise
+    */
+    bool init(UINT filterSize,UINT numDimensions);
+    
+    /**
+    Filters the input, this should only be called if the dimensionality of the filter was set to 1.
+    
+    @param x: the value to filter, this should only be called if the dimensionality of the filter was set to 1
+    @return the filtered value.  Zero will be returned if the value was not filtered
+    */
+    Float filter(const Float x);
+    
+    /**
+    Filters the input, the dimensionality of the input vector should match that of the filter.
+    
+    @param x: the values to filter, the dimensionality of the input vector should match that of the filter
+    @return the filtered values.  An empty vector will be returned if the values were not filtered
+    */
+    VectorFloat filter(const VectorFloat &x);
+    
+    /**
+    Gets the current filter size.
+    
+    @return returns the filter size
+    */
+    UINT getFilterSize() const { return filterSize; }
+    
+    /**
+    Returns the last value(s) that were filtered.
+    
+    @return the filtered values.  An empty vector will be returned if the values were not filtered
+    */
+    VectorFloat getFilteredData() const { return processedData; }
+    
+    //Tell the compiler we are using the following functions from the MLBase class to stop hidden virtual function warnings
+    using MLBase::save;
+    using MLBase::load;
+    
+protected:
+    UINT filterSize;                                        ///< The size of the filter
+    UINT inputSampleCounter;                                ///< A counter to keep track of the number of input samples
+    CircularBuffer< VectorFloat > dataBuffer;           ///< A buffer to store the previous N values, N = filterSize
+    
+    static RegisterPreProcessingModule< RMSFilter > registerModule;
+};
+
+GRT_END_NAMESPACE
