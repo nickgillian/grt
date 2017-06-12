69cd42a051109bdd7fb91836ef0a5610fa024215
Nick Gillian <nick@nickgillian.com>
2017-06-11 21:03:30 -0700

    Added pipeline example unit test.
    Updated style to 2 spaces.

--- GRT/DataStructures/VectorFloat.cpp
+++ GRT/DataStructures/VectorFloat.cpp
@@ -25,250 +25,232 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 GRT_BEGIN_NAMESPACE
    
 VectorFloat::VectorFloat(){
-    warningLog.setKey("[WARNING VectorFloat]");
-    errorLog.setKey("[ERROR VectorFloat]");
+  warningLog.setKey("[WARNING VectorFloat]");
+  errorLog.setKey("[ERROR VectorFloat]");
 }
     
 VectorFloat::VectorFloat(const size_type size){
-    warningLog.setKey("[WARNING VectorFloat]");
-    errorLog.setKey("[ERROR VectorFloat]");
-    resize( size );
+  warningLog.setKey("[WARNING VectorFloat]");
+  errorLog.setKey("[ERROR VectorFloat]");
+  resize( size );
 }
 
-VectorFloat::VectorFloat( const size_type size, const Float &value ){
-    warningLog.setKey("[WARNING VectorFloat]");
-    errorLog.setKey("[ERROR VectorFloat]");
-    resize( size, value );
+VectorFloat::VectorFloat(const size_type size, const Float &value){
+  warningLog.setKey("[WARNING VectorFloat]");
+  errorLog.setKey("[ERROR VectorFloat]");
+  resize(size, value);
 }
     
 VectorFloat::VectorFloat(const VectorFloat &rhs):Vector(rhs){
-    warningLog.setKey("[WARNING VectorFloat]");
-    errorLog.setKey("[ERROR VectorFloat]");
+  warningLog.setKey("[WARNING VectorFloat]");
+  errorLog.setKey("[ERROR VectorFloat]");
 }
 
 VectorFloat::~VectorFloat(){
-    clear();
+  clear();
 }
     
 VectorFloat& VectorFloat::operator=(const VectorFloat &rhs){
-    if( this != &rhs ){
-        UINT N = rhs.getSize();
-        if( N > 0 ){
-            resize( N );
-            std::copy( rhs.begin(), rhs.end(), this->begin() );
-        }else this->clear();
-    }
-    return *this;
+  if (this != &rhs) {
+    UINT N = rhs.getSize();
+    if (N > 0) {
+      resize(N);
+      std::copy(rhs.begin(), rhs.end(), this->begin());
+    } else this->clear();
+  }
+  return *this;
 }
     
 VectorFloat& VectorFloat::operator=(const Vector< Float > &rhs){
-    if( this != &rhs ){
-        UINT N = rhs.getSize();
-        if( N > 0 ){
-            resize( N );
-            std::copy( rhs.begin(), rhs.end(), this->begin() );
-        }else this->clear();
-    }
-    return *this;
+  if (this != &rhs) {
+    UINT N = rhs.getSize();
+    if (N > 0) {
+      resize(N);
+      std::copy(rhs.begin(), rhs.end(), this->begin());
+    } else this->clear();
+  }
+  return *this;
 }
   
 bool VectorFloat::print(const std::string title) const {
-    
-    if( title != "" ){
-        std::cout << title << std::endl;
-    }
-    const size_type size = this->size();
-    const Float *data = getData();
-    for(size_type i=0; i<size; i++){
-        std::cout << data[i] << "\t";
-    }
-    std::cout << std::endl;
-
-    return true;
+  if (title != "") {
+    std::cout << title << std::endl;
+  }
+  const size_type size = this->size();
+  const Float *data = getData();
+  for (size_type i=0; i<size; i++) {
+      std::cout << data[i] << "\t";
+  }
+  std::cout << std::endl;
+  return true;
 }
     
-bool VectorFloat::scale( const Float minTarget, const Float maxTarget, const bool constrain ){
-    
-    MinMax range = getMinMax();
-    
-    return scale( range.minValue, range.maxValue, minTarget, maxTarget, constrain );
+bool VectorFloat::scale(const Float minTarget, const Float maxTarget, const bool constrain) {
+  MinMax range = getMinMax();
+  return scale(range.minValue, range.maxValue, minTarget, maxTarget, constrain);
 }
     
-bool VectorFloat::scale( const Float minSource, const Float maxSource, const Float minTarget, const Float maxTarget, const bool constrain ){
-    
-    const size_type N = this->size();
-    
-    if( N == 0 ){
-        return false;
-    }
-    
-    size_type i = 0;
-    Float *data = getData();
-    for( i=0; i<N; i++ ){
-        data[i] = grt_scale(data[i],minSource,maxSource,minTarget,maxTarget,constrain);
-    }
-
-    return true;
+bool VectorFloat::scale(const Float minSource, const Float maxSource, const Float minTarget, const Float maxTarget, const bool constrain){  
+  const size_type N = this->size();
+  
+  if (N == 0) {
+    return false;
+  }
+  
+  size_type i = 0;
+  Float *data = getData();
+  for (i=0; i<N; i++) {
+    data[i] = grt_scale(data[i],minSource,maxSource,minTarget,maxTarget,constrain);
+  }
+  return true;
 }
     
-Float VectorFloat::getMinValue() const{
-    Float minValue = 99e+99;
-    const size_type N = this->size();
-    const Float *data = getData();
-    for(size_type i=0; i<N; i++){
-        if( data[i] < minValue ) minValue = data[i];
-    }
-    return minValue;
+Float VectorFloat::getMinValue() const {
+  Float minValue = std::numeric_limits<Float>::max();
+  const size_type N = this->size();
+  const Float *data = getData();
+  for (size_type i=0; i<N; i++) {
+    if (data[i] < minValue) minValue = data[i];
+  }
+  return minValue;
 }
 
 Float VectorFloat::getMaxValue() const{
-    Float maxValue = -99e99;
-    const size_type N = this->size();
-    const Float *data = getData();
-    for(size_type i=0; i<N; i++){
-        if( data[i] > maxValue ) maxValue = data[i];
-    }
-    return maxValue;
+  Float maxValue = -std::numeric_limits<Float>::max();
+  const size_type N = this->size();
+  const Float *data = getData();
+  for (size_type i=0; i<N; i++) {
+    if (data[i] > maxValue) maxValue = data[i];
+  }
+  return maxValue;
 }
     
 Float VectorFloat::getMean() const {
-    
-    Float mean = 0.0;
-    const size_type N = this->size();
-    const Float *data = getData();
-    for(size_type i=0; i<N; i++){
-        mean += data[i];
-    }
-    mean /= N;
-    
-    return mean;
+  Float mean = 0.0;
+  const size_type N = this->size();
+  if (N==0) return mean;
+  const Float *data = getData();
+  for (size_type i=0; i<N; i++) {
+    mean += data[i];
+  }
+  mean /= N;
+  return mean;
 }
     
 Float VectorFloat::getStdDev() const {
-    
-    Float mean = getMean();
-	Float stdDev = 0.0;
-    const size_type N = this->size();
-    const Float *data = getData();
-	
-	for(size_type i=0; i<N; i++){
+  Float stdDev = 0.0;
+  const size_type N = this->size();
+  if (N==0) return stdDev;
+  const Float mean = getMean();
+  const Float *data = getData();
+	for (size_type i=0; i<N; i++) {
 		stdDev += grt_sqr(data[i]-mean);
 	}
-    stdDev = grt_sqrt( stdDev / Float(N-1) );
-
-    return stdDev;
+  stdDev = grt_sqrt(stdDev / Float(N-1));
+  return stdDev;
 }
     
 MinMax VectorFloat::getMinMax() const {
-
-    const size_type N = this->size();
-    MinMax range;
-    
-    if( N == 0 ) return range;
-
-    const Float *data = getData();
-    
-    for(size_type i=0; i<N; i++){
-        range.updateMinMax( data[ i ] );
-    }
-
-    return range;
+  MinMax range;
+  const size_type N = this->size();
+  if (N == 0) return range;
+  const Float *data = getData();
+  for (size_type i=0; i<N; i++) {
+    range.updateMinMax(data[i]);
+  }
+  return range;
 }
   
 bool VectorFloat::save(const std::string &filename) const {
-
-    const size_type N = this->size();
-
-    if( N == 0 ){
-        warningLog << "save(...) - Vector is empty, nothing to save!" << std::endl;
-        return false;
-    }
-    
-    std::fstream file;
-    file.open(filename.c_str(), std::ios::out);
-    
-    if( !file.is_open() ){
-        return false;
-    }
-    
-    const Float *data = getData();
-    for(size_type i=0; i<N; i++){
-        file << data[i] << (i<N-1 ? "," : "\n");
-    }
-    
-    file.close();
-
-    return true;
+  const size_type N = this->size();
+  if (N == 0) {
+    warningLog << "Vector is empty, nothing to save!" << std::endl;
+    return false;
+  }
+  
+  std::fstream file;
+  file.open(filename.c_str(), std::ios::out);
+  if (!file.is_open()) {
+    errorLog << "Failed to open file: " << filename << std::endl;
+    return false;
+  }
+  
+  const Float *data = getData();
+  for (size_type i=0; i<N; i++) {
+    file << data[i] << (i<N-1 ? "," : "\n");
+  }
+  file.close();
+  return true;
 }
     
-bool VectorFloat::load(const std::string &filename,const char seperator){
+bool VectorFloat::load(const std::string &filename, const char seperator) {
     
-    //Clear any previous data
-    clear();
+  //Clear any previous data
+  clear();
 
-    //Open the file
-    std::ifstream file( filename.c_str(), std::ifstream::in );
-    if ( !file.is_open() ){
-        warningLog << "load(...) - Failed to open file: " << filename << std::endl;
-        return false;
-    }
-    
-    Vector< std::string > vec;
-    Vector< Float > row;
-    std::string line;
-    std::string columnString = "";
-    const int sepValue = seperator;
-    unsigned int numElements = 0;
-    size_t length = 0;
-    
-    //Get the data, for a vector it should just be one line
-    if( !getline(file,line) ){
-        warningLog << "load(...) - Failed to read first row!" << std::endl;
-        return false;
-    }
-    
-    //Scan the line contents for the seperator token and parse the contents between each token
-    columnString = "";
-    length = line.length();
-    vec.reserve( length );
-    for(size_t i=0; i<length; i++){
-        if( int(line[i]) == sepValue ){
-            vec.push_back( columnString );
-            columnString = "";
-        }else columnString += line[i];
-    }
+  //Open the file
+  std::ifstream file( filename.c_str(), std::ifstream::in );
+  if (!file.is_open()){
+    warningLog << "Failed to open file: " << filename << std::endl;
+    return false;
+  }
+  
+  Vector< std::string > vec;
+  Vector< Float > row;
+  std::string line;
+  std::string columnString = "";
+  const int sepValue = seperator;
+  unsigned int numElements = 0;
+  size_t length = 0;
+  
+  //Get the data, for a vector it should just be one line
+  if (!getline(file,line)) {
+    warningLog << "Failed to read first row!" << std::endl;
+    return false;
+  }
+  
+  //Scan the line contents for the seperator token and parse the contents between each token
+  columnString = "";
+  length = line.length();
+  vec.reserve( length );
+  for (size_t i=0; i<length; i++) {
+    if (int(line[i]) == sepValue) {
+      vec.push_back(columnString);
+      columnString = "";
+    }else columnString += line[i];
+  }
         
-    //Add the last column
-    vec.push_back( columnString );
-            
-    //Remove the new line character from the string in the last column
-    if( vec.size() >= 1 ){
-        size_t K = vec.size()-1;
-        size_t foundA = vec[ K ].find('\n');
-        size_t foundB = vec[K ].find('\r');
-        if( foundA != std::string::npos || foundB != std::string::npos ){
-            vec[ K ] = vec[ K ].substr(0,vec[ K ].length()-1);
-        }
+  //Add the last column
+  vec.push_back( columnString );
+          
+  //Remove the new line character from the string in the last column
+  if( vec.size() >= 1 ){
+    size_t K = vec.size()-1;
+    size_t foundA = vec[ K ].find('\n');
+    size_t foundB = vec[K ].find('\r');
+    if (foundA != std::string::npos || foundB != std::string::npos) {
+        vec[ K ] = vec[ K ].substr(0,vec[ K ].length()-1);
     }
-    
-    numElements = vec.getSize();
-    
-    //Assign the memory
-    if( !resize( numElements ) ){
-        warningLog << "load(...) - Failed to resize memory!" << std::endl;
-        return false;
-    }
-        
-    //Convert the string column values to Float values
-    Float *data = getData();
-    for(unsigned int i=0; i<numElements; i++){
-        data[i] = grt_from_str< Float >( vec[i] );
-    }
-    
-    //Close the file
-    file.close();
-    
-    return true;
+  }
+  
+  numElements = vec.getSize();
+  
+  //Assign the memory
+  if (!resize(numElements)) {
+    warningLog << "Failed to resize memory!" << std::endl;
+    return false;
+  }
+      
+  //Convert the string column values to Float values
+  Float *data = getData();
+  for (unsigned int i=0; i<numElements; i++) {
+    data[i] = grt_from_str< Float >( vec[i] );
+  }
+  
+  //Close the file
+  file.close();
+  
+  return true;
 }
     
 GRT_END_NAMESPACE
--- tests/DataStructures/VectorFloatUnitTest.cpp
+++ tests/DataStructures/VectorFloatUnitTest.cpp
@@ -13,14 +13,14 @@ TEST(VectorFloat, DefaultConstructor) {
 // Tests the resize c'tor.
 TEST(VectorFloat, ResizeConstructor) {
 	const UINT size = 100;
-	VectorFloat vec( size );
+	VectorFloat vec(size);
 	EXPECT_EQ(size, vec.getSize());
 }
 
 // Tests the copy c'tor.
 TEST(VectorFloat, CopyConstructor) {
 	const UINT size = 100;
-	VectorFloat vec1( size );
+	VectorFloat vec1(size);
 	EXPECT_EQ(size, vec1.getSize());
 	VectorFloat vec2( vec1 );
 	EXPECT_EQ(vec1.getSize(), vec2.getSize());
@@ -29,7 +29,7 @@ TEST(VectorFloat, CopyConstructor) {
 // Tests the equals operator.
 TEST(VectorFloat, EqualsConstructor) {
 	const UINT size = 100;
-	VectorFloat vec1( size );
+	VectorFloat vec1(size);
 	EXPECT_EQ(size, vec1.getSize());
 	VectorFloat vec2;
 	vec2 = vec1;
@@ -39,7 +39,7 @@ TEST(VectorFloat, EqualsConstructor) {
 // Tests the Vector< Float > equals operator.
 TEST(VectorFloat, VecFloatEqualsConstructor) {
 	const UINT size = 100;
-	Vector< Float > vec1( size );
+	Vector< Float > vec1(size);
 	EXPECT_EQ(size, vec1.getSize());
 	VectorFloat vec2;
 	vec2 = vec1;
@@ -49,22 +49,22 @@ TEST(VectorFloat, VecFloatEqualsConstructor) {
 // Tests the save and load methods.
 TEST(VectorFloat, SaveLoad) {
 	const UINT size = 100;
-	VectorFloat vec1( size );
+	VectorFloat vec1(size);
 	EXPECT_EQ(size, vec1.getSize());
-	for(UINT i=0; i<size; i++){ vec1[i] = i*1.0; }
+	for (UINT i=0; i<size; i++) { vec1[i] = i*1.0; }
 
 	//Save the vector to a CSV file
-	EXPECT_TRUE( vec1.save("vec.csv") );
+	EXPECT_TRUE(vec1.save("vec.csv"));
 	
 	//Load the data from the file into another vector
 	VectorFloat vec2;
-	EXPECT_TRUE( vec2.load("vec.csv") );
+	EXPECT_TRUE(vec2.load("vec.csv"));
 
 	//Vector 2 should now be the same size as vector 1
 	EXPECT_EQ(vec1.getSize(), vec2.getSize());
 
 	//Check to make sure the values match
-	for(UINT i=0; i<size; i++){ EXPECT_EQ(vec1[i], vec2[i]); }
+	for (UINT i=0; i<size; i++) { EXPECT_EQ(vec1[i], vec2[i]); }
 }
 
 // Tests the scale function
@@ -72,103 +72,102 @@ TEST(VectorFloat, Scale) {
 	const UINT size = 100;
 	VectorFloat vec( size );
 	EXPECT_EQ(size, vec.getSize());
-	for(UINT i=0; i<size; i++){ vec[i] = i*1.0; }
+	for (UINT i=0; i<size; i++) { vec[i] = i*1.0; }
 
 	//Scale the contents in the vector
-	Float minTarget = -1.0f;
-	Float maxTarget = 1.0f;
-	EXPECT_TRUE( vec.scale(minTarget,maxTarget) );
+	const Float minTarget = -1.0f;
+	const Float maxTarget = 1.0f;
+	EXPECT_TRUE(vec.scale(minTarget,maxTarget));
 
-	EXPECT_EQ( vec.getMinValue(), minTarget );
-	EXPECT_EQ( vec.getMaxValue(), maxTarget );
+	EXPECT_EQ(vec.getMinValue(), minTarget);
+	EXPECT_EQ(vec.getMaxValue(), maxTarget);
 }
 
 // Tests the min-max scale function
 TEST(VectorFloat, MinMaxScale) {
 	const UINT size = 100;
-	VectorFloat vec( size );
+	VectorFloat vec(size);
 	EXPECT_EQ(size, vec.getSize());
-	for(UINT i=0; i<size; i++){ vec[i] = i*1.0; }
+	for (UINT i=0; i<size; i++) { vec[i] = i*1.0; }
 
 	//Scale the contents in the vector
 	Float minSource = 10; //Deliberately set the min source to 10, even though the min value should be 0
 	Float maxSource = 90; //Deliberately set the max source to 90, even though the max value should be 100
 	Float minTarget = -1.0f;
 	Float maxTarget = 1.0f;
-	EXPECT_TRUE( vec.scale(minSource, maxSource, minTarget, maxTarget, true) );
-	EXPECT_EQ( vec.getMinValue(), minTarget );
-	EXPECT_EQ( vec.getMaxValue(), maxTarget );
+	EXPECT_TRUE(vec.scale(minSource, maxSource, minTarget, maxTarget, true));
+	EXPECT_EQ(vec.getMinValue(), minTarget);
+	EXPECT_EQ(vec.getMaxValue(), maxTarget);
 
 	//Reset the data
-	for(UINT i=0; i<size; i++){ vec[i] = i*1.0; }
-	EXPECT_TRUE( vec.scale(minSource, maxSource, minTarget, maxTarget, false) ); //Re-run the scaling with constraining off
-	EXPECT_TRUE( fabs( vec.getMinValue() - minTarget ) > 0.01 ); //The min value should no longer match the min target
-	EXPECT_TRUE( fabs( vec.getMaxValue() - maxTarget ) > 0.01 ); //The max value should no longer match the max target
+	for (UINT i=0; i<size; i++) { vec[i] = i*1.0; }
+	EXPECT_TRUE(vec.scale(minSource, maxSource, minTarget, maxTarget, false)); //Re-run the scaling with constraining off
+	EXPECT_TRUE(fabs( vec.getMinValue() - minTarget ) > 0.01 ); //The min value should no longer match the min target
+	EXPECT_TRUE(fabs( vec.getMaxValue() - maxTarget ) > 0.01 ); //The max value should no longer match the max target
 }
 
 // Tests the getMinValue
 TEST(VectorFloat, GetMin) {
 	const UINT size = 100;
-	VectorFloat vec( size );
+	VectorFloat vec(size);
 	EXPECT_EQ(size, vec.getSize());
-	for(UINT i=0; i<size; i++){ vec[i] = i*1.0; }
-	EXPECT_EQ( vec.getMinValue(), 0.0f );
+	for (UINT i=0; i<size; i++) { vec[i] = i*1.0; }
+	EXPECT_EQ(vec.getMinValue(), 0.0f);
 }
 
 // Tests the getMaxValue
 TEST(VectorFloat, GetMax) {
 	const UINT size = 100;
-	VectorFloat vec( size );
+	VectorFloat vec(size);
 	EXPECT_EQ(size, vec.getSize());
-	for(UINT i=0; i<size; i++){ vec[i] = i*1.0; }
-	EXPECT_EQ( vec.getMaxValue(), 99.0f );
+	for (UINT i=0; i<size; i++) { vec[i] = i*1.0; }
+	EXPECT_EQ(vec.getMaxValue(), 99.0f);
 }
 
 // Tests the getMean
 TEST(VectorFloat, GetMean) {
 	const UINT size = 100;
-	VectorFloat vec( size );
+	VectorFloat vec(size);
 	EXPECT_EQ(size, vec.getSize());
-	for(UINT i=0; i<size; i++){ vec[i] = i*1.0; }
+	for (UINT i=0; i<size; i++) { vec[i] = i*1.0; }
 	Float mean = 0.0;
-	for(UINT i=0; i<size; i++){
+	for (UINT i=0; i<size; i++) {
 		mean += vec[i];
 	}
 	mean /= size;
-	EXPECT_EQ( vec.getMean(), mean );
+	EXPECT_EQ(vec.getMean(), mean);
 }
 
 // Tests the getStdDev
 TEST(VectorFloat, GetStdDev) {
 	const UINT size = 100;
-	VectorFloat vec( size );
+	VectorFloat vec(size);
 	EXPECT_EQ(size, vec.getSize());
-	for(UINT i=0; i<size; i++){ vec[i] = i*1.0; }
+	for (UINT i=0; i<size; i++) { vec[i] = i*1.0; }
 	Float mean = 0.0;
 	Float stddev = 0.0;
-	for(UINT i=0; i<size; i++){
+	for (UINT i=0; i<size; i++) {
 		mean += vec[i];
 	}
 	mean /= size;
-	for(UINT i=0; i<size; i++){
-		stddev += grt_sqr( vec[i]-mean );
+	for (UINT i=0; i<size; i++) {
+		stddev += grt_sqr(vec[i]-mean);
 	}
-	stddev = grt_sqrt( stddev / (size-1) );
-
-	EXPECT_EQ( vec.getStdDev(), stddev );
+	stddev = grt_sqrt(stddev / (size-1));
+	EXPECT_EQ(vec.getStdDev(), stddev);
 }
 
 // Tests the getMinMax
 TEST(VectorFloat, GetMinMax) {
 	const UINT size = 100;
-	VectorFloat vec( size );
+	VectorFloat vec(size);
 	EXPECT_EQ(size, vec.getSize());
-	for(UINT i=0; i<size; i++){ vec[i] = i*1.0; }
-	Float expectedMin = 0.0;
-	Float expectedMax = size-1;
-	MinMax result = vec.getMinMax();
-	EXPECT_EQ( expectedMin, result.minValue );
-	EXPECT_EQ( expectedMax, result.maxValue );
+	for (UINT i=0; i<size; i++) { vec[i] = i*1.0; }
+	const Float expectedMin = 0.0;
+	const Float expectedMax = size-1;
+	const MinMax result = vec.getMinMax();
+	EXPECT_EQ(expectedMin, result.minValue);
+	EXPECT_EQ(expectedMax, result.maxValue);
 }
 
 int main(int argc, char **argv) {
