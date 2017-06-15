03b393b9bff32603d18e3669ac0823e4c07570ff
Nick Gillian <nick@nickgillian.com>
2017-06-14 22:14:39 -0700

    Updated MLP to use normalized initialization for setting neuron weights.

--- GRT/RegressionModules/ArtificialNeuralNetworks/MLP/Neuron.h
+++ GRT/RegressionModules/ArtificialNeuralNetworks/MLP/Neuron.h
@@ -47,7 +47,7 @@ public:
 
     Neuron& operator=(const Neuron &rhs);
     
-    bool init(const UINT numInputs,const Type actvationFunction,Random &random,const Float minWeightRange = -0.1, const Float maxWeightRange = 0.1,const Float minBiasRange = -0.1,const Float maxBiasRange = 0.1);
+    bool init(const UINT numInputs, const Type actvationFunction, Random &random, const Float minWeightRange = -0.1, const Float maxWeightRange = 0.1, const Float minBiasRange = -0.1, const Float maxBiasRange = 0.1);
     void clear();
     Float fire(const VectorFloat &x);
     Float getDerivative(const Float &y);
